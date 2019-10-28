da323fec4d5438fc5684935ea1233885cebcb52b
Chanakarn Niyornram <chana@digitalnatives.nl>
2019-10-28 14:36:37 +0100

    prettier

--- src/components/atom/form/ZanadouForm.vue
+++ src/components/atom/form/ZanadouForm.vue
@@ -13,88 +13,88 @@
 
 <script>
 export default {
-	name: 'Form',
-	data() {
-		return {
-			email: ''
-		}
-	},
-	computed: {
-		isActive() {
-			if (this.email !== '') {
-				return true
-			} else {
-				return false
-			}
-		}
-	},
-	methods: {
-		clicked() {
-			this.$emit('click', this.email)
-		}
-	}
+    name: 'Form',
+    data() {
+        return {
+            email: ''
+        }
+    },
+    computed: {
+        isActive() {
+            if (this.email !== '') {
+                return true
+            } else {
+                return false
+            }
+        }
+    },
+    methods: {
+        clicked() {
+            this.$emit('click', this.email)
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .container-form {
-	width: 20rem;
+    width: 20rem;
 }
 
 .form {
-	display: flex;
-	flex-wrap: wrap;
+    display: flex;
+    flex-wrap: wrap;
 
-	label {
-		position: relative;
+    label {
+        position: relative;
 
-		display: flex;
-		flex-wrap: wrap;
-		width: 100%;
-		justify-content: center;
-		align-items: center;
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
 
-		&:focus-within {
-			.label {
-				transform: translate(0);
-				font-size: 1rem;
-				color: black;
-			}
-		}
-	}
+        &:focus-within {
+            .label {
+                transform: translate(0);
+                font-size: 1rem;
+                color: black;
+            }
+        }
+    }
 }
 
 .label {
-	width: 100%;
-	transform: translate(1rem, 2.4rem);
-	transition: transform 0.1s ease-out;
-	pointer-events: none;
-	color: rgb(144, 144, 144);
-	font-size: 1.2rem;
+    width: 100%;
+    transform: translate(1rem, 2.4rem);
+    transition: transform 0.1s ease-out;
+    pointer-events: none;
+    color: rgb(144, 144, 144);
+    font-size: 1.2rem;
 
-	&.active {
-		transform: translate(0);
-		font-size: 1rem;
-		color: black;
-	}
+    &.active {
+        transform: translate(0);
+        font-size: 1rem;
+        color: black;
+    }
 }
 
 .input {
-	width: 100%;
-	margin-top: 0.4rem;
-	padding: 0.5rem 1rem;
+    width: 100%;
+    margin-top: 0.4rem;
+    padding: 0.5rem 1rem;
 
-	font-size: 1.4rem;
+    font-size: 1.4rem;
 }
 
 .button {
-	margin-top: 1rem;
-	padding: 1rem;
-	font-size: 1.1rem;
-	font-weight: bold;
-	background-color: #dd5762;
-	width: 100%;
-	color: white;
-	border: none;
+    margin-top: 1rem;
+    padding: 1rem;
+    font-size: 1.1rem;
+    font-weight: bold;
+    background-color: #dd5762;
+    width: 100%;
+    color: white;
+    border: none;
 }
 </style>
